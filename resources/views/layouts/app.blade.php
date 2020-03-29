d066b5c2034bb53614d5d81c8a8dc6e3a17b51f9
B0013067 <B00130967@mytudublin.ie>
2020-03-29 15:55:02 +0200

    bundeld everything up

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,12 +3,16 @@
 <!DOCTYPE html>
     <head> 
         <meta charset="UTF-8"> 
-        <title>Title TennisClub</title> 
+        <title>TennisClub</title> 
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'> 
         <!-- Bootstrap 3.3.7 --> 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="https://use.fontawesome.com/0fa7cf6792.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="{{asset('css/app.css')}}"> 
+        <script src="{{asset('js/app.js')}}"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> 
+
+
+
+
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> 
         <link rel="stylesheet" href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css"> 
     </head> 
